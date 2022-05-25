f9cf1298cb53d407a6b555997de591f3ffdb84de
Alan Brandi <alan.brandi@hotmail.com>
2022-05-25 20:08:01 -0300

    player funcional

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    public UnityEvent<Vector3> OnMouseClickInteractable;
     NavMeshAgent agent;
     MouseManager manager;
     Animator animator;
+    public float magnitude;
 
     private void Start()
     {
@@ -18,6 +21,7 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
+        magnitude = agent.velocity.magnitude;
         animator.SetFloat("Speed", agent.velocity.magnitude);
     }
     public void Move(Vector3 Location)
