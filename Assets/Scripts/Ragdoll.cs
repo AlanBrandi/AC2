0298ff327990cac36e2911dffa30088002e95e1d
Alan Brandi <alan.brandi@hotmail.com>
2022-06-07 12:31:22 -0300

    Level novo

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    public Ray ray;
+    public LayerMask mask;
+    public GameObject BloodFX;
+    public float bulletForce = 1000;
+
+    AudioSource audioDie;
+
+    private void Start()
+    {
+        audioDie = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray, out hit, 20, mask))
+        {
+           audioDie.Play();
+
+            hit.collider.GetComponent<Rigidbody>().AddForce(ray.direction * bulletForce);
+            Instantiate(BloodFX, hit.point, Quaternion.LookRotation(hit.normal), hit.collider.transform);
+            enabled = false;
+        }
+    }
+}
